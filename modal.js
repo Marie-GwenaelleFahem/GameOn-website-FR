f4b7bfa657b35d1218ba299ed2b18b6fee1cac04
Marie-Gwenaëlle Fahem <mgfahem@gmail.com>
2021-08-04 22:54:10 +0200

    checking inputs done

--- modal.js
+++ modal.js
@@ -12,13 +12,15 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modal = document.getElementById('modal');
+const first = document.getElementById('first');
+const registrationForm = document.querySelector('.registration-form');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-formData.addEventListener("click", validatePrenom());
-formData.addEventListener("click", validateNom());
-formData.addEventListener("click", validateEmail());
-formData.addEventListener("click", validateBirthdate());
+//first.addEventListener("onchange", validatePrenom());
+//formData.addEventListener("click", validateNom());
+//formData.addEventListener("click", validateEmail());
+//formData.addEventListener("click", validateBirthdate());
 
 // close modal
 function modalDisplay(displayStyle) {
@@ -31,13 +33,12 @@ window.onclick = function(event) {
 }
 
 // checking inputs functions
-/*function validatePrenom() {
-  // regex 
+
+// Vérifie si le champ prénom possède au moins 2 caractères
+function validatePrenom() {
   let checkString = /^[a-zA-Z]{2}/;
-  // récupération des éléments du formulaire
   let prenom = document.getElementById("first");
   let error = document.getElementById("error-prenom");
-  // tester si les conditions sont remplies
   if(checkString.test(prenom.value) === false) {
     prenom.classList.add("input-error");
     error.innerText = "Veuillez entrer 2 caractères ou plus dans le champ du prénom";
@@ -48,8 +49,9 @@ window.onclick = function(event) {
     prenom.innerText = "";
     return true;
   }
-}*/
+}
 
+// Vérifie si le champ nom possède au moins 2 caractères
 function validateNom() {
   let checkString = /^[a-zA-Z]{2}/;
   let nom = document.getElementById("last");
@@ -66,6 +68,7 @@ function validateNom() {
   }
 }
 
+// Vérifie si le champ email est valide
 function validateEmail() {
   let checkMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   let email = document.getElementById("email");
@@ -83,7 +86,7 @@ function validateEmail() {
 }
 
 function validateBirthdate() {
-  let checkDate = /^\d{2}-\d{2}-\d{4}$/;
+  let checkDate = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
   let birthdate = document.getElementById("birthdate");
   let error = document.getElementById("error-birthdate");
     if (checkDate.test(birthdate.value) === false) {
@@ -91,21 +94,61 @@ function validateBirthdate() {
     return false;
     }
     error.innerText = "";
+    birthdate.classList.add("input-validate");
       return true;
   }
 
+function validateQuantity() {
+  let checkNumber = /^[0-9]$/;
+  let quantity = document.getElementById("quantity");
+  let error = document.getElementById("error-quantity");
+    if (checkNumber.test(quantity.value) === false) {
+      quantity.innerText = "Veuillez entrer un nombre";
+      return false;
+    }
+    error.innerText = "";
+    quantity.classList.add("input-validate");
+      return true;
+}
+
 function validateCity() {
   let cities = document.querySelectorAll("input[type=radio]");
-  let error = document.getElementById("error_city");
+  let error = document.getElementById("error-city");
   for (i = 0; i < cities.length; i++) {
     if(cities[i].checked) {
       error.innerText = "";
       return true;
     }
-    cities.innerText = "Veuillez choisir une option";
+    error.innerText = "Veuillez choisir une option";
     return false;
   }
 }
+
+function validateTerms() {
+  let terms = document.querySelector('#acceptConditions');
+  let error = document.getElementById('error-terms');
+  let termsAreChecked = terms.checked;
+  if (termsAreChecked) {
+    terms.classList.add("input-error");
+    error.innerText = "Vous devez accepter les conditions d'utilisation";
+  } else {
+    terms.classList.remove("input-error");
+    error.innerText = "";
+  }
+  return termsAreChecked;
+}
+
+// Fonction qui vérifie que tout les champs sont valides
+const isFormValid = () => validatePrenom() && validateNom() && validateEmail() && validateBirthdate() && validateQuantity() && validateCity() && validateTerms() 
+
+//EventListener pour le bouton submit
+registrationForm.addEventListener('submit', function(event) {
+  event.preventDefault()
+  if (isFormValid()) {
+    
+
+  }
+})
 
 // launch modal form
 function launchModal() {
