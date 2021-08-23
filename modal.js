ca17c02fbedcf68b282d03c57a9c2760fe9ba4a3
Marie-Gwenaëlle Fahem <mgfahem@gmail.com>
2021-08-23 20:46:37 +0200

    update

--- modal.js
+++ modal.js
@@ -13,7 +13,6 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modal = document.getElementById('modal');
 const first = document.getElementById('first');
-/*const registrationForm = document.querySelector('.registration-form');*/
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -85,6 +84,7 @@ function validateEmail() {
   }
 }
 
+// Vérifie la validité de la date de naissance
 function validateBirthdate() {
   let checkDate = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
   let birthdate = document.getElementById("birthdate");
@@ -100,6 +100,7 @@ function validateBirthdate() {
       return true;
   }
 
+// Vérifie qu'un nombre soit entré dans le champs correspondant
 function validateQuantity() {
   let checkNumber = /^0*(?:[1-9][0-9]?|100)$/;
   let quantity = document.getElementById("quantity");
@@ -115,6 +116,7 @@ function validateQuantity() {
       return true;
 }
 
+// Vérifie qu'une ville soit séléctionnée 
 function validateCity() {
   let cities = document.querySelectorAll("input[type=radio]");
   let error = document.getElementById("error-city");
@@ -128,10 +130,11 @@ function validateCity() {
     return false;
   }
 
+// Vérifie que les conditions d'utilisations soient acceptées  
 function validateTerms() {
   let terms = document.querySelector('#acceptConditions');
   let error = document.getElementById('error-terms');
-  if (terms.checked === true ) {
+  if (terms.checked === true) {
     error.innerText = "";
     return true;
   } else {
@@ -140,32 +143,41 @@ function validateTerms() {
   }
 }
 
-//Fonction 
+//Fonction qui valide les informations du formulaire et affiche un message
 function validateForm (event) {
-
-  if (validatePrenom() === false ) {
+  if (validatePrenom() === false) {
     return false;
   }
-  if (validateNom() === false ) {
+  if (validateNom() === false) {
     return false;
   }
-  if (validateEmail() === false ) {
+  if (validateEmail() === false) {
     return false;
   }
-  if (validateBirthdate() === false ) {
+  if (validateBirthdate() === false) {
     return false;
   }
-  if (validateQuantity() === false ) {
+  if (validateQuantity() === false) {
     return false;
   }
-  if (validateCity() === false ) {
+  if (validateCity() === false) {
     return false;
   }
-  if (validateTerms() === false ) {
+  if (validateTerms() === false) {
     return false;
   } else {
     event.preventDefault();
-
+    formData.remove();
+    let modal = document.querySelector("div.modal-body");
+    let message = document.createElement("p");
+    message.classList.add("message-validation");
+    message.textContent = "Merci ! Votre réservation a été reçue";
+    modal.appendChild(message);
+    let btnCloseModal = document.createElement("button");
+    btnCloseModal.classList.add("button-submit");
+    btnCloseModal.textContent = "Fermer";
+    btnCloseModal.addEventListener("click", closeModal);
+    modal.appendChild(btnCloseModal);
   }
 }
 
